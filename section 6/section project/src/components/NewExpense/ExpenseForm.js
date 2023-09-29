ad145137acd12f7796d382beaaeddcaa7c91ba71
pkindalov <panayotkindalov@gmail.com>
2023-09-29 19:38:54 +0300

    Condition content - lecturer version

--- section 6/section project/src/components/NewExpense/ExpenseForm.js
+++ section 6/section project/src/components/NewExpense/ExpenseForm.js
@@ -91,7 +91,10 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
-        <button onClick={handleCancelAddingExpense}>Cancel</button>
+        {/* My Version
+        <button onClick={handleCancelAddingExpense}>Cancel</button> */}
+
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
--- section 6/section project/src/components/NewExpense/NewExpense.js
+++ section 6/section project/src/components/NewExpense/NewExpense.js
@@ -4,23 +4,40 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
-  const [isFormShown, setFormShown] = useState(false);
+  // My version
+  // const [isFormShown, setFormShown] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
 
-  const handleShowForm = (isShown = false) => {
-    setFormShown(isShown);
+  //My version
+  // const handleShowForm = (isShown = false) => {
+  //   setFormShown(isShown);
+  // }
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   }
 
   return (
     <div className='new-expense'>
-      {isFormShown && <ExpenseForm onShowForm={handleShowForm} onSaveExpenseData={saveExpenseDataHandler} />}
-      {!isFormShown && <button onClick={() => handleShowForm(true)}>Add New Expense</button>}
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
+
+
+      {/* My version */}
+      {/* {isFormShown && <ExpenseForm onShowForm={handleShowForm} onSaveExpenseData={saveExpenseDataHandler} />}
+      {!isFormShown && <button onClick={() => handleShowForm(true)}>Add New Expense</button>} */}
     </div>
   );
 };
