c31b0fb6d8395fd5991565ae92d893452781ba53
pkindalov <panayotkindalov@gmail.com>
2023-10-01 13:01:05 +0300

    Small changes and formatting code

--- section 9/01-starting-project/src/App.js
+++ section 9/01-starting-project/src/App.js
@@ -5,20 +5,19 @@ import { useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
-  const noDataMessage = <p className="no-data-msg">No investment calculated yet</p>;
+  const noDataMessage = (
+    <p className="no-data-msg">No investment calculated yet</p>
+  );
   const calculateHandler = (userInput) => {
-    
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
-    
+
     const yearlyData = []; // per-year results
 
     let currentSavings = +userInput["currentSavings"]; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput["yearlyContribution"]; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput["expectedReturn"] / 100;
     const duration = +userInput["duration"];
-
-    // console.log(currentSavings, yearlyContribution, expectedReturn, duration);
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
@@ -34,7 +33,6 @@ function App() {
     }
 
     // do something with yearlyData ...
-    console.log(yearlyData);
     setData(yearlyData);
   };
 
@@ -42,14 +40,9 @@ function App() {
     <div>
       <Header />
       <NewInvestmentForm onSubmit={calculateHandler} />
-
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
-      {data.length > 0 ? (
-        <InvestmentsList data={data} />
-      ) : (
-        noDataMessage
-      )}
+      {data.length > 0 ? <InvestmentsList data={data} /> : noDataMessage}
     </div>
   );
 }
--- section 9/01-starting-project/src/components/Investments/InvestmentsList.js
+++ section 9/01-starting-project/src/components/Investments/InvestmentsList.js
@@ -1,28 +1,39 @@
-import styles from './InvestmentsList.module.css';
+import styles from "./InvestmentsList.module.css";
 
 const InvestmentsList = ({ data }) => {
-    return (
-        <table className={styles.result}>
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
+  return (
+    <table className={styles.result}>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((investment, index) => {
+          console.log(investment);
+          return (
+            <tr key={index}>
+              <td>{investment.year}</td>
+              <td>{investment.savingsEndOfYear}</td>
+              <td>{investment.yearlyInterest}</td>
+              <td>{investment.yearlyInterest}</td>
+            </tr>
+          );
+        })}
+        {/* <tr>
             <td>YEAR NUMBER</td>
             <td>TOTAL SAVINGS END OF YEAR</td>
             <td>INTEREST GAINED IN YEAR</td>
             <td>TOTAL INTEREST GAINED</td>
             <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
-    )
-}
+          </tr> */}
+      </tbody>
+    </table>
+  );
+};
 
 export default InvestmentsList;
