5d6a1abe14e291afdbc122f3c667174bf30925bb
pkindalov <panayotkindalov@gmail.com>
2023-10-01 21:20:46 +0300

    Fixing a small bug

--- section 9/First Practice Project Lecturer Version/src/components/UserInput/UserInput.js
+++ section 9/First Practice Project Lecturer Version/src/components/UserInput/UserInput.js
@@ -22,7 +22,7 @@ const UserInput = (props) => {
 
   const inputChangeHandler = (input, value) => {
     setUserInput((prevInput) => {
-      return { ...prevInput, [input]: value };
+      return { ...prevInput, [input]: +value };
     });
   };
 
