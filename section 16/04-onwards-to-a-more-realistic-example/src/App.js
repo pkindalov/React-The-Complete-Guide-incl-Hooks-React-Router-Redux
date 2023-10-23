6e744772e283cc456bdac45f6bc5588677f52cf3
pkindalov <panayotkindalov@gmail.com>
2023-10-23 19:50:35 +0300

    Building a Custom Http Hook and formatting code

--- section 16/04-onwards-to-a-more-realistic-example/src/App.js
+++ section 16/04-onwards-to-a-more-realistic-example/src/App.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
+  const fetchTasks = async () => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        "https://react-http-77e98-default-rtdb.firebaseio.com/tasks.json"
       );
 
       if (!response.ok) {
-        throw new Error('Request failed!');
+        throw new Error("Request failed!");
       }
 
       const data = await response.json();
@@ -30,7 +30,7 @@ function App() {
 
       setTasks(loadedTasks);
     } catch (err) {
-      setError(err.message || 'Something went wrong!');
+      setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
   };
