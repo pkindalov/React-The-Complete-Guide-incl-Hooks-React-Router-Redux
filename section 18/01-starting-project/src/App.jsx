a9377661eb3ddfa3ea8aa7c6644e20d49fe62c55
pkindalov <panayotkindalov@gmail.com>
2023-11-07 13:31:49 +0200

    Fetching Meals Data (GET HTTP Request)

--- section 18/01-starting-project/src/App.jsx
+++ section 18/01-starting-project/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header.jsx";
+import Meals from "./components/Meals/Meals.jsx";
 
 function App() {
     return (
         <>
             <Header/>
+            <Meals/>
         </>
     );
 }
---
+++ section 18/01-starting-project/src/components/Meals/Meals.jsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+
+const Meals = () => {
+    const [loadedMeals, setLoadedMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch("http://localhost:3000/meals");
+            if (!response.ok) {
+                //...
+            }
+            const meals = await response.json();
+            setLoadedMeals(meals);
+        };
+
+        fetchMeals();
+
+    }, []);
+
+
+    return (
+        <ul id="meals">
+            {loadedMeals.map((meal) => (
+                <li key={meal.id}>{meal.name}</li>
+            ))}
+        </ul>
+    )
+}
+
+export default Meals;
