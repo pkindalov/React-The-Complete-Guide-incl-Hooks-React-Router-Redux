a02023b71bca397674910b4c47d653fe165cac1f
pkindalov <panayotkindalov@gmail.com>
2023-11-16 09:21:12 +0200

    Sending a POST Request with Order Data

--- section 18/01-starting-project/src/components/Checkout/Checkout.jsx
+++ section 18/01-starting-project/src/components/Checkout/Checkout.jsx
@@ -26,6 +26,19 @@ export default function Checkout() {
     //extracting values form inputs
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries()); // ex.. { email: "test@example.com" }
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
@@ -33,7 +46,7 @@ export default function Checkout() {
       <form onSubmit={handleSubmit} action="#">
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
