c2325dbae4b0f3dc02f00450ba335940eb3f8530
pkindalov <panayotkindalov@gmail.com>
2023-11-27 10:04:00 +0200

    Migrating Everything To Redux Toolkit

--- section 19/01-starting-project/src/components/Counter.js
+++ section 19/01-starting-project/src/components/Counter.js
@@ -1,4 +1,5 @@
 import {useSelector, useDispatch, connect} from "react-redux";
+import {counterActions} from "../store/index";
 import classes from './Counter.module.css';
 import {Component} from "react";
 
@@ -8,25 +9,18 @@ const Counter = () => {
     const show = useSelector(state => state.showCounter);
 
     const incrementHandler = () => {
-        dispatch({
-            type: 'increment',
-        });
+        dispatch(counterActions.increment());
     };
 
     const increaseHandler = () => {
-        dispatch({
-            type: "increase",
-            amount: 5
-        });
+        dispatch(counterActions.increase(5)); // {type: SOME_UNIQUE_IDENTIFIER, payload: 5}
     }
 
     const decrementHandler = () => {
-        dispatch({
-            type: 'decrement',
-        })
+        dispatch(counterActions.decrement());
     };
     const toggleCounterHandler = () => {
-        dispatch({type: "toggle"})
+        dispatch(counterActions.toggleCounter());
     };
 
     return (
--- section 19/01-starting-project/src/store/index.js
+++ section 19/01-starting-project/src/store/index.js
@@ -6,17 +6,19 @@ const initialState = {counter: 0, showCounter: true};
 const counterSlice = createSlice({
     name: 'counter',
     initialState,
-    increment(state) {
-        state.counter++;
-    },
-    decrement(state) {
-        state.counter--;
-    },
-    increase(state, action) {
-        state.counter = state.counter + action.amount;
-    },
-    toggleCounter(state) {
-        state.showCounter = !state.showCounter;
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
     }
 });
 
@@ -25,4 +27,5 @@ const store = configureStore({
     reducer: counterSlice.reducer
 });
 
+export const counterActions = counterSlice.actions;
 export default store;
