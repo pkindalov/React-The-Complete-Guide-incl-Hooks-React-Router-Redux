a8c02bcd7e6c91188564dc1c19763d84e8cd95b4
pkindalov <panayotkindalov@gmail.com>
2023-12-07 10:33:17 +0200

    Getting Started with Fetching Data

--- section 20/Addimg redux to project - lecturer version/src/App.js
+++ section 20/Addimg redux to project - lecturer version/src/App.js
@@ -5,7 +5,7 @@ import Products from "./components/Shop/Products";
 // import { uiActions } from "./store/ui-slice";
 import { Fragment, useEffect } from "react";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -14,6 +14,10 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if (isInitial) {
