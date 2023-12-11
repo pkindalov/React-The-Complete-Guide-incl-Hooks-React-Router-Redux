057411c62b481e95ec25490276c44ea32e183b89
pkindalov <panayotkindalov@gmail.com>
2023-12-11 10:01:21 +0200

    Layouts & Nested Routes

--- section 21/01-starting-project 2/src/App.js
+++ section 21/01-starting-project 2/src/App.js
@@ -1,6 +1,7 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 //Old way to define routes(before react router 6.4)
 // const routeDefinitions = createRoutesFromElements(
@@ -13,12 +14,18 @@ import ProductsPage from "./pages/Products";
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-    {path: '/', element: <HomePage/>},
-    {path: '/products', element: <ProductsPage/>},
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
