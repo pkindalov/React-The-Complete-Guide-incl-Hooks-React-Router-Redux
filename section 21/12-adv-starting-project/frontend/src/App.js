65bd01b357cebdde00c9651455af707fae857a89
pkindalov <panayotkindalov@gmail.com>
2023-12-18 09:00:42 +0200

    Small changes

--- section 21/12-adv-starting-project/frontend/src/App.js
+++ section 21/12-adv-starting-project/frontend/src/App.js
@@ -34,10 +34,14 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "events", element: <EventsPage /> },
-      { path: "events/:id", element: <EventDetailPage /> },
-      { path: "events/new", element: <NewEventPage /> },
-      { path: "events/:id/edit", element: <EditEventPage /> },
+      {
+        path: "events",
+        element: <EventsPage />,
+        children: [],
+      },
+      { path: "/events/:id", element: <EventDetailPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "/events/:id/edit", element: <EditEventPage /> },
     ],
   },
 ]);
