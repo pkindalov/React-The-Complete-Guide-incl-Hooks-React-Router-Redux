58d22017ea50281ce91ec8910bebd9d58d7add78
pkindalov <panayotkindalov@gmail.com>
2023-12-22 09:42:54 +0200

    The json() Utility Function

--- section 21/after exercise/frontend/src/pages/Events.js
+++ section 21/after exercise/frontend/src/pages/Events.js
@@ -1,5 +1,5 @@
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import {json, useLoaderData} from "react-router-dom";
 
 function EventsPage() {
   const data = useLoaderData();
@@ -22,9 +22,18 @@ export async function loader() {
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
     //   throw {message: 'Could not fetch events.'};
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+
+
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+
+      throw json(
+          { message: "Could not fetch events." },
+          {
+              status: 500,
+          },
+      );
   } else {
     return response;
   }
