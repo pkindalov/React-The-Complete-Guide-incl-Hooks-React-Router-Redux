22ce3612c64febe9930a45f21fa665b5b88c62fa
pkindalov <panayotkindalov@gmail.com>
2023-12-22 09:28:59 +0200

    Extracting Error Data & Throwing Responses

--- section 21/after exercise/frontend/src/pages/Error.js
+++ section 21/after exercise/frontend/src/pages/Error.js
@@ -1,5 +1,29 @@
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
 function ErrorPage() {
-  return <h1>An error occurred!</h1>;
+  const error = useRouteError();
+  let title = "An error occurred";
+  let message = "Something went wrong";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not Found";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
 
 export default ErrorPage;
