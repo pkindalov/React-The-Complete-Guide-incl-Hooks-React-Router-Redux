fe318e5e48f584c8820f29f0323dbe74d0f2d5ad
pkindalov <panayotkindalov@gmail.com>
2023-12-22 12:26:33 +0200

    The useRouteLoaderData() Hook & Accessing Data From Other Routes

--- section 21/after exercise/frontend/src/App.js
+++ section 21/after exercise/frontend/src/App.js
@@ -46,11 +46,18 @@ const router = createBrowserRouter([
           { index: true, element: <EventsPage />, loader: eventsLoader },
           {
             path: ":eventId",
-            element: <EventDetailPage />,
+            id: "event-detail",
             loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+              },
+              { path: "edit", element: <EditEventPage /> },
+            ],
           },
+
           { path: "new", element: <NewEventsPage /> },
-          { path: ":eventId/edit", element: <EditEventPage /> },
         ],
       },
     ],
