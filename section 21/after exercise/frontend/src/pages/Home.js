cb0a30e77e8ea6931df8dba89500c1e5fd609214
pkindalov <panayotkindalov@gmail.com>
2024-01-06 12:53:35 +0200

    Last small changes for the section

--- section 21/after exercise/frontend/src/pages/Home.js
+++ section 21/after exercise/frontend/src/pages/Home.js
@@ -1,5 +1,11 @@
+import PageContent from "../components/PageContent";
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <PageContent title="Wellcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 }
 
 export default HomePage;
